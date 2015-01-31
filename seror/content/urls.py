052c27e2ae89b90056654bdeb66b48ed73de2fdc
Davor Sauer <davor@backbase.com>
2015-01-31 12:03:37 +0100

    basic notify and test endpoints

--- seror/content/urls.py
+++ seror/content/urls.py
@@ -3,8 +3,10 @@
 from django.conf.urls import patterns, url
 
 from .views import PageView
+from .views import TestView
 
 urlpatterns = patterns ('',
-    url(r'', PageView.as_view(), name='page'),
+    url(r'/notify', PageView.as_view(), name='page'),
+    url(r'/test', TestView.as_view(), name='page'),
 
 )
